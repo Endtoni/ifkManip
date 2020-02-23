223b2e3416d134ec5eeb85f6b8122865563e0cee
TaoYe <endtoni@gmail.com>
2020-02-23 00:55:18 -0800

    clean u

--- ifkManip.cpp
+++ ifkManip.cpp
@@ -1,23 +1,25 @@
 #include <maya/MIOStream.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <maya/MFn.h>
-#include <maya/MPxNode.h>
-#include <maya/MPxManipContainer.h>
-#include <maya/MPxSelectionContext.h>
-#include <maya/MPxContextCommand.h>
-#include <maya/MModelMessage.h>
-#include <maya/MFnPlugin.h>
-#include <maya/MGlobal.h>
-#include <maya/MItSelectionList.h>
+#include <maya/MFnFreePointTriadManip.h>
 #include <maya/MPoint.h>
 #include <maya/MVector.h>
-#include <maya/MDagPath.h>
 #include <maya/MManipData.h>
 #include <maya/MMatrix.h>
-// Manipulators
-#include <maya/MFnFreePointTriadManip.h>
-#include <maya/MFnDistanceManip.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MPlug.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFnPlugin.h>
+#include <maya/MPxManipContainer.h>
+#include <maya/MGlobal.h>
+#include <maya/MPxNode.h>
+#include <maya/MDagPath.h>
+#include <maya/MFn.h>
+#include <maya/MPxSelectionContext.h>
+#include <maya/MModelMessage.h>
+#include <maya/MPxContextCommand.h>
+#include <maya/MGlobal.h>
+
 
 class IfkManip : public MPxManipContainer
 {
@@ -35,12 +37,34 @@ public:
 		M3dView::DisplayStatus status) override;
 private:
 	void updateManipLocations(const MObject &node);
+
 public:
 	MDagPath fDistanceManip;
 	MDagPath fFreePointManip;
 	static MTypeId id;
 };
+
+class IfkManipContext : public MPxSelectionContext
+{
+public:
+	IfkManipContext();
+	void    toolOnSetup(MEvent &event) override;
+	void    toolOffCleanup() override;
+	static void updateManipulators(void * data);
+private:
+	MCallbackId id1;
+};
+
+class IfkManipContextCmd : public MPxContextCommand
+{
+public:
+	IfkManipContextCmd() {};
+	MPxContext * makeObj() override;
+public:
+	static void* creator();
+};
 MTypeId IfkManip::id(0x8001d);
+
 IfkManip::IfkManip()
 {
 	// The constructor must not call createChildren for user-defined
@@ -51,16 +75,20 @@ IfkManip::~IfkManip()
 }
 void *IfkManip::creator()
 {
+	MGlobal::displayInfo("Create IFK mani");
+
 	return new IfkManip();
 }
 MStatus IfkManip::initialize()
 {
+	MGlobal::displayInfo("init ifk");
 	MStatus stat;
 	stat = MPxManipContainer::initialize();
 	return stat;
 }
 MStatus IfkManip::createChildren()
 {
+	MGlobal::displayInfo("create children");
 	MStatus stat = MStatus::kSuccess;
 	fDistanceManip = addDistanceManip("distanceManip",
 		"distance");
@@ -68,11 +96,8 @@ MStatus IfkManip::createChildren()
 	return stat;
 }
 void IfkManip::updateManipLocations(const MObject &node)
-//
-// Description
-//        setTranslation and setRotation to the parent's transformation.
-//
-{
+{	
+	MGlobal::displayInfo("update");
 	MFnDagNode dagNodeFn(node);
 	MDagPath nodePath;
 	dagNodeFn.getPath(nodePath);
@@ -89,14 +114,9 @@ void IfkManip::updateManipLocations(const MObject &node)
 MStatus IfkManip::connectToDependNode(const MObject &node)
 {
 	MStatus stat;
-	//
-	// This routine connects the distance manip to the scaleY plug on the node
-	// and connects the translate plug to the position plug on the freePoint
-	// manipulator.
-	//
+	MGlobal::displayInfo("connected to dependNode");
 	MFnDependencyNode nodeFn(node);
 	MPlug tPlug = nodeFn.findPlug("translate", &stat);
-	MFnDistanceManip distanceManipFn(fDistanceManip);
 	MFnFreePointTriadManip freePointManipFn(fFreePointManip);
 	freePointManipFn.connectToPointPlug(tPlug);
 	updateManipLocations(node);
@@ -109,35 +129,17 @@ void IfkManip::draw(M3dView & view,
 	M3dView::DisplayStyle style,
 	M3dView::DisplayStatus status)
 {
-	//
-	// Demonstrate how drawing can be overriden for manip containers - draw the
-	// string "Stretch Me!" at the origin.
-	//
+	MGlobal::displayInfo("draw");
 	MPxManipContainer::draw(view, path, style, status);
 	view.beginGL();
 	MPoint textPos(0, 0, 0);
 	char str[100];
-	sprintf_s(str, "Ik translateManip");
+	sprintf_s(str, "Ik translate Manip");
 	MString distanceText(str);
 	view.drawText(distanceText, textPos, M3dView::kLeft);
 	view.endGL();
 }
-//
-// MoveManipContext
-//
-// This class is a simple context for supporting a move manipulator.
-//
-class IfkManipContext : public MPxSelectionContext
-{
-public:
-	IfkManipContext();
-	void    toolOnSetup(MEvent &event) override;
-	void    toolOffCleanup() override;
-	// Callback issued when selection list changes
-	static void updateManipulators(void * data);
-private:
-	MCallbackId id1;
-};
+
 IfkManipContext::IfkManipContext()
 {
 	MString str("Plugin move Manipulator");
@@ -168,7 +170,7 @@ void IfkManipContext::toolOffCleanup()
 void IfkManipContext::updateManipulators(void * data)
 {
 	MStatus stat = MStatus::kSuccess;
-
+	MGlobal::displayInfo("Update manip");
 	IfkManipContext * ctxPtr = (IfkManipContext *)data;
 	ctxPtr->deleteManipulators();
 	MSelectionList list;
@@ -199,8 +201,10 @@ void IfkManipContext::updateManipulators(void * data)
 			//
 			MString manipName("ifkManip");
 			MObject manipObject;
+
 			IfkManip* manipulator =
 				(IfkManip *)IfkManip::newManipulator(manipName, manipObject);
+
 			if (NULL != manipulator) {
 				// Add the manipulator
 				//
@@ -213,50 +217,38 @@ void IfkManipContext::updateManipulators(void * data)
 						" object: " + dependNodeFn.name());
 				}
 			}
+			else {
+				MGlobal::displayError("Failed to create manipulator object");
+			}
 		}
 	}
 }
-//
-// moveManipContext
-//
-// This is the command that will be used to create instances
-// of our context.
-//
-class IfkManipContextCmd : public MPxContextCommand
-{
-public:
-	IfkManipContextCmd() {};
-	MPxContext * makeObj() override;
-public:
-	static void* creator();
-};
+
 MPxContext *IfkManipContextCmd::makeObj()
 {
 	return new IfkManipContext();
 }
+
 void *IfkManipContextCmd::creator()
 {
 	return new IfkManipContextCmd;
 }
-//
-// The following routines are used to register/unregister
-// the context and manipulator
-//
+
 MStatus initializePlugin(MObject obj)
 {
 	MStatus status;
-	MFnPlugin plugin(obj, PLUGIN_COMPANY, "3.0", "Any");
+	MFnPlugin plugin(obj, "TYE", "1.0", "Any");
 	status = plugin.registerContextCommand("ifkContext",
 		&IfkManipContextCmd::creator);
 	if (!status) {
-		MGlobal::displayError("Error registering moveManipContext command");
+		MGlobal::displayError("Error registering ifkManipContext command");
 		return status;
 	}
-	status = plugin.registerNode("moveManip", IfkManip::id,
+	status = plugin.registerNode("ifkManpulator", IfkManip::id,
 		&IfkManip::creator, &IfkManip::initialize,
 		MPxNode::kManipContainer);
 	if (!status) {
-		MGlobal::displayError("Error registering moveManip node");
+		MGlobal::displayError("Error registering ifkManpulator node");
 		return status;
 	}
 	return status;
@@ -267,12 +259,12 @@ MStatus uninitializePlugin(MObject obj)
 	MFnPlugin plugin(obj);
 	status = plugin.deregisterContextCommand("ifkContext");
 	if (!status) {
-		MGlobal::displayError("Error deregistering moveManipContext command");
+		MGlobal::displayError("Error deregistering ifkManipContext command");
 		return status;
 	}
 	status = plugin.deregisterNode(IfkManip::id);
 	if (!status) {
-		MGlobal::displayError("Error deregistering moveManip node");
+		MGlobal::displayError("Error deregistering ifkManpulator node");
 		return status;
 	}
 	return status;
